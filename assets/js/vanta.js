9d6b195afbf47cb7d1d1ebae0fe3824d09d1c21a
Keerthana Jayakumar <keetu.jk@gmail.com>
2021-07-04 20:44:59 -0400

    Adding portfolio with vanta

---
+++ assets/js/vanta.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#trunk-vanta",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xff3f99,
+  backgroundColor: 0x322b39,
+  points: 5.00,
+  maxDistance: 17.00,
+  spacing: 11.00
+})
